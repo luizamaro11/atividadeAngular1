6bf02761642957830f7d6b96a2f584738f73f49a
luizamaro11 <luiz.amaro90@gmail.com>
2019-10-17 17:43:52 -0300

    construindo o layout inserindo o bootstrap

--- src/app/somar/somar.module.ts
+++ src/app/somar/somar.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SomarComponent } from './components/somar/somar.component';
-
-
+import { SomarComponent } from './components/somar';
+import { SomarService } from './services';
 
 @NgModule({
   declarations: [SomarComponent],
   imports: [
     CommonModule
+  ],
+  providers: [
+    SomarService
   ]
 })
 export class SomarModule { }
