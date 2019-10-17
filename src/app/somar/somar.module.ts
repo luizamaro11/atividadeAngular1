578c2fce5f4f0dfe710df403c7f79771a2e7293e
luizamaro11 <luiz.amaro90@gmail.com>
2019-10-17 17:25:34 -0300

    implementacao do component somar

--- src/app/somar/somar.module.ts
+++ src/app/somar/somar.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SomarComponent } from './components/somar/somar.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [SomarComponent],
   imports: [
     CommonModule
   ]
